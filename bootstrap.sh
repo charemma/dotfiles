de38a9f2083d8547b942491f160812de941a288a
Charalambos Emmanouilidis <charalambos.emmanouilidis@gmail.com>
2025-04-17 07:45:10 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,38 +1,15 @@
 #!/bin/bash
 
-# bootstrap.sh can be used to install dotfiles with chemzoi in a devcontainer
-set -e
+# This script is used to bootstrap a new system, especially in devcontainer setups.
 
-# Check if brew exists, install locally if missing (no shell integration)
-if ! command -v brew &>/dev/null; then
-  echo "Homebrew not found. Installing temporarily for local use..."
-  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Install chezmoi and dotfiles
+export BINDIR=~/.local/bin
+sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply --skip-existing charemma
 
-  # Manually set brew path for this session
-  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-fi
-
-
-# Use brew to install required packages (local only)
-echo "Installing packages via Homebrew..."
-brew install nvim chezmoi fzf ripgrep yazi tmux direnv lsd bat glow tig npm
-
-# Run chezmoi init to set up the dotfiles
-echo "Initializing chezmoi "
-chezmoi init https://github.com/charemma/dotfiles.git
-chezmoi update
-echo "✔️ Dotfiles installed."
-
-# set path to zsh in /etc/passwd
-ZSH_PATH=$(which zsh)
-if ! grep -q "$ZSH_PATH" /etc/passwd; then
-  echo "Updating /etc/passwd to set zsh as the default shell..."
-  sudo sed -i "s|/bin/bash|$ZSH_PATH|g" /etc/passwd
-fi
-echo "✔️ Default shell updated to zsh."
-
-
-bat cache --build
+# Install zsh and oh-my-zsh
 test -d $HOME/.oh-my-zsh || sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-echo "✔️ Bootstrap complete. Restart your shell if needed."
+# Setup bat
+if [ -f $(which bat) ]; then
+    bat cache --build
+fi
