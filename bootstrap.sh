dff1ebbb7b5a1282bb204c311d3b9223bdcc5e06
Charalambos Emmanouilidis <charalambos.emmanouilidis@gmail.com>
2025-04-17 08:04:18 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,8 @@
 # This script is used to bootstrap a new system, especially in devcontainer setups.
 
 # Install chezmoi and dotfiles
-sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply charemma
+export BINDIR=~/.local/bin
+sh -c "$(curl -fsLS get.chezmoi.io)" -- init --force --apply charemma
 
 # Install zsh and oh-my-zsh
 test -d $HOME/.oh-my-zsh || sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
