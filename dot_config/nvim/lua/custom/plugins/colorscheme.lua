72815fe6397272866e6f6cb1ce9c2b93b29b440d
Charalambos Emmanouilidis <charalambos.emmanouilidis@gmail.com>
2025-06-17 21:48:18 +0200

    feat(nvim): Enable auto colorscheme (dark/light) selection

--- dot_config/nvim/lua/custom/plugins/colorscheme.lua
+++ dot_config/nvim/lua/custom/plugins/colorscheme.lua
@@ -1,10 +1,31 @@
 return {
   'catppuccin/nvim',
   priority = 1000, -- Make sure to load this before all the other start plugins.
+
   init = function()
-    -- Load the colorscheme here.
-    -- vim.cmd.colorscheme 'catppuccin-frappe'
-    vim.cmd.colorscheme 'catppuccin-latte'
+    require('catppuccin').setup {
+      flavour = 'auto', -- latte, frappe, macchiato, mocha
+      background = { -- :h background
+        light = 'latte',
+        dark = 'frappe',
+      },
+      default_integrations = true,
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = false,
+        mini = {
+          enabled = true,
+          indentscope_color = '',
+        },
+        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+      },
+    }
+
+    -- setup must be called before loading
+    vim.cmd.colorscheme 'catppuccin'
 
     -- You can configure highlights by doing something like:
     vim.cmd.hi 'Comment gui=none'
