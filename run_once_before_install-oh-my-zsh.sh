f3bfa08c98bb170cf0358f27e01041943f62c300
Charalambos Emmanouilidis <charalambos.emmanouilidis@proton.me>
2026-03-01 08:07:07 +0200

    shebangs auf env bash umstellen fuer NixOS kompatibilitaet

--- run_once_before_install-oh-my-zsh.sh
+++ run_once_before_install-oh-my-zsh.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
